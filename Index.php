ba075ee92fc9a7de681daf95cfebc835c9212c2a
Rinacans <94143245+Rinacans@users.noreply.github.com>
2021-11-12 03:51:24 +0700

    Create Index.php

---
+++ Index.php
@@ -0,0 +1,49 @@
+<html>
+<!-- Anon7 Was Here -->
+<!-- AnonSec Team -->
+
+<!-- if you read this admin, patch your security now
+ your server have many bug, if you didn't patch it on 3 month from now, something bad will happens -->
+ 
+<head>
+    <title>Hacked by Arya Xploit</title>
+    <meta charset="UTF-8"/>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1" /> 
+    <meta property="og:title" content="Hacked by Arya Xploit">
+    <meta name="description" content="wh00pz ! kpu, your security get down">
+    <meta property="og:description" content="wh00pz ! kpu, your security get down">
+    <meta property="og:image" content="https://i.imgur.com/6RSyvoJ.jpg">
+    <meta name="theme-color" content="#fff">
+    <style type='text/css'>body,a:hover{cursor:url(../cur.cursors-4u.net/symbols/sym-1/sym46.cur),progress!important;}</style>
+</head>
+<body bgcolor="black" text="white" oncontextmenu="return false;" onkeydown="return false;" onmousedown="return false;" onclick="play()">
+<style type="text/css">
+    center {
+        font-family: Courier;
+    }
+    img {
+        opacity: 80%;
+    }
+    red {
+        color: red;
+    }
+</style>
+<script language="JavaScript">
+    window.onbeforeunload = confirmExit;
+    function confirmExit() {
+        return "are you sure ? wkwk";
+    }
+</script>
+<table width="100%" height="80%">
+    <td>
+        <center>
+            <small></small><br>
+            <img src="https://b.top4top.io/p_2135x5i4v9.png" width="100" height="150">
+            <br>Hacked by <black><i>Skyzi Xploit</i></red><br><font size="2">BUKAN<br>KAMU YANG SALAH TAPI AKU YANG TERLALU BERHARAP HEMMM:)<br></font><br><audio autoplay="autoplay" controls="controls" src="https://f.top4top.io/m_21324bzf11.mp4" type="audio/mpeg"></audio>
+            <br>
+            <br><small><font size="2" color="white">CYBER MEGURI FUJIURA | SEVEN GHOST TEAM</font></small><br><br><br>
+            
+
+    </td>
+</table>
